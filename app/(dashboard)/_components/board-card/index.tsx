a2fb8fe702e2dbc0f1f35cf70ec207b9bc2972a3
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-07-12 12:29:21 +0500

    feat:/ Fetching Boards from Backend

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+const BoardCard = ({
+  id,
+  title,
+  authorName,
+  authorId,
+  createdAt,
+  imageUrl,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-center overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BoardCard;
