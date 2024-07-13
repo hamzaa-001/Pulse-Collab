fd33eb0fb6aedfb780d2fe0225fe5b710d9a8172
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-07-13 18:50:12 +0500

    feat:/ Card Actions

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,8 +8,7 @@ import { useAuth } from "@clerk/nextjs";
 import Footer from "./Footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { MoreHorizontal } from "lucide-react";
-import Actions from "@/components/actions";
-
+import Actions from "@/components/Actions";
 interface BoardCardProps {
   id: string;
   title: string;
@@ -46,10 +45,11 @@ const BoardCard = ({
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
           {/* //@ts-ignore */}
-          <Actions id={id} title={title} side="right" />
-          <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
-            <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
-          </button>
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
--- components/actions.tsx
+++ components/actions.tsx
@@ -8,6 +8,13 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
+import ConfirmModal from "./ConfirmModal";
+import { Link2, Pencil, Trash } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { api } from "@/convex/_generated/api";
+import { Button } from "./ui/button";
+import { useRenameModal } from "@/store/useRenameModal";
 
 interface actionsProps {
   children: React.ReactNode;
@@ -18,9 +25,57 @@ interface actionsProps {
 }
 
 const Actions = ({ children, side, sideOffset, id, title }: actionsProps) => {
+  const { onOpen } = useRenameModal();
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board Deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link Copied"))
+      .catch(() => toast.error("Failed to copy Link"));
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy Board Link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="p-3 cursor-pointer"
+          onClick={() => onOpen(id, title)}
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete Board?"
+          description="This will delete the board and all of its contents. Are you sure?"
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            variant="ghost"
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
