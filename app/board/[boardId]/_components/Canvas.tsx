0d165455a0ae4b5ff41b9dc76b9ae6893ac73455
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-07-24 12:22:31 +0500

    feat:/ Added Room participants, Toolbar icons Selection, Canvas State

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import { useSelf } from "@liveblocks/react";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/type/canvas";
 import Info from "./Info";
 import Participants from "./Participants";
 import Toolbar from "./Toolbar";
+import { useCanRedo, useCanUndo, useHistory } from "@liveblocks/react";
 
 interface CanvasProps {
   boardId: string;
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   return (
     <main className="h-screen w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canUndo={canUndo}
+        canRedo={canRedo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
--- app/board/[boardId]/_components/Participants.tsx
+++ app/board/[boardId]/_components/Participants.tsx
@@ -1,10 +1,50 @@
+"use client";
+
+import { useOthers, useSelf } from "@liveblocks/react";
+
+import UserAvatar from "./userAvatar";
 import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
+import { connectionIdColor } from "@/lib/utils";
+
+const MAX_SHOWN_USER = 1;
 
 const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USER;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List os users
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USER).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "A"}
+            />
+          );
+        })}
+
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser?.info?.name} (You)`}
+            fallback={currentUser?.info?.name?.[0] || "A"}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USER} more`}
+            fallback={`+${users.length - MAX_SHOWN_USER}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
