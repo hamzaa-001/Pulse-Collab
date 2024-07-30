aa34fa108d259c2b9c736adcd6e2677f3e92b9e7
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-07-30 12:29:14 +0500

    fix:/ Small Fixes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "./_components/Sidebar";
 import OrganisationSidebar from "./_components/OrganisationSidebar";
 import Navbar from "./_components/Navbar";
+import { RedirectToSignUp, SignedOut, SignedIn } from "@clerk/nextjs";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -8,18 +9,25 @@ interface DashboardLayoutProps {
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <main className="h-screen">
-      <Sidebar />
-      <div className="pl-[60px] h-screen">
-        <div className="flex gap-x-3 h-screen">
-          <OrganisationSidebar />
-          <div className="h-screen flex-1">
-            <Navbar />
-            {children}
+    <>
+      <SignedIn>
+        <main className="h-screen">
+          <Sidebar />
+          <div className="pl-[60px] h-screen">
+            <div className="flex gap-x-3 h-screen">
+              <OrganisationSidebar />
+              <div className="h-screen flex-1">
+                <Navbar />
+                {children}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </main>
+        </main>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignUp />
+      </SignedOut>
+    </>
   );
 };
 
